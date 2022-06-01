76e328dabb64948855f6be7fd1a73d8ef42e5738
Facundo Toledo <toledo.facundon@gmail.com>
2022-06-01 20:40:08 -0300

    commit numero 8

---
+++ demo/demo/src/main/java/com/example/demo/entidades/Veterinario.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.entidades;
+
+
+public class Veterinario {
+    
+    private String nombre;
+    private String apellido;
+    private String matricula;
+
+    public Veterinario() {
+    }
+
+    public Veterinario(String nombre, String apellido, String matricula) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    
+    
+}
